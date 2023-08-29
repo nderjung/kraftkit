c97073dcdffbdd581bb4270c743312bba2730de3
Alexander Jung <alex@unikraft.io>
2023-08-29 12:32:05 +0000

    feat!: Introduce dynamic initramfs serialization for root filesystems
    This commit introduces a refactor of the mechanics behind serializing an
    input "rootfs" such that it is a usable initramfs.  This change starts
    with the adjustment of the `initrd` package which is now based on an
    interface `Initrd` which is used to introduce several underlying
    implementations that can serialize a provided input.  The following
    possible inputs are:
    
    - A path to a directory which will act as the rootfs;
    - A `Dockerfile` which can be built via buildkit; and,
    - For compatibility of the interface, an existing file representing a
      CPIO archive.
    
    The adjustment to this interface has been made across the project
    including relevant tools.  Importantly, the GitHub Action has also been
    updated to set up BuildKit as a "sidecar" via the composite type.
    
    Signed-off-by: Alexander Jung <alex@unikraft.io>

--- oci/pack.go
+++ oci/pack.go
@@ -47,7 +47,7 @@ type ociPackage struct {
 	plat    plat.Platform
 	kconfig kconfig.KeyValueMap
 	kernel  string
-	initrd  *initrd.InitrdConfig
+	initrd  initrd.Initrd
 	command []string
 }
 
@@ -180,31 +180,9 @@ func NewPackageFromTarget(ctx context.Context, targ target.Target, opts ...packm
 
 	if popts.Initrd() != "" {
 		log.G(ctx).Debug("oci: including initrd")
-		initRdPath := popts.Initrd()
-		if f, err := os.Stat(initRdPath); err == nil && f.IsDir() {
-			cwd, err2 := os.Getwd()
-			if err2 != nil {
-				return nil, err2
-			}
-
-			file, err := os.CreateTemp("", "kraftkit-oci-archive-*")
-			if err != nil {
-				return nil, err
-			}
-			defer os.Remove(file.Name())
-
-			cfg, err2 := initrd.NewFromMapping(cwd,
-				file.Name(),
-				fmt.Sprintf("%s:/", initRdPath))
-			if err2 != nil {
-				return nil, err2
-			}
-
-			initRdPath = cfg.Output
-		}
 		layer, err := NewLayerFromFile(ctx,
 			ocispec.MediaTypeImageLayer,
-			initRdPath,
+			popts.Initrd(),
 			WellKnownInitrdPath,
 			WithLayerAnnotation(AnnotationKernelInitrdPath, WellKnownInitrdPath),
 		)
@@ -510,7 +488,7 @@ unpack:
 		// Set the initrd if available
 		initrdPath := filepath.Join(popts.Workdir(), WellKnownInitrdPath)
 		if f, err := os.Stat(initrdPath); err == nil && f.Size() > 0 {
-			ocipack.initrd, err = initrd.NewFromFile(popts.Workdir(), initrdPath)
+			ocipack.initrd, err = initrd.New(ctx, initrdPath)
 			if err != nil {
 				return err
 			}
@@ -571,7 +549,7 @@ func (ocipack *ociPackage) KernelDbg() string {
 }
 
 // Initrd implements unikraft.target.Target
-func (ocipack *ociPackage) Initrd() *initrd.InitrdConfig {
+func (ocipack *ociPackage) Initrd() initrd.Initrd {
 	return ocipack.initrd
 }
 
